92ebf2994cfb88739f41429963e8b7391e4d07ce
Spandana N <nspandana18@gmail.com>
2017-04-27 11:26:10 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,25 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+Parse.Cloud.define('All', function(request, status)  
+{
+  var query=new Parse.Query("MyCollection");
+   query.find().then(function (res) 
+{
+for (var i=0; i< res.length;i++){
+var expirydate=res[i].get('PExpirationDate');
+ 
+var inputDate = new Date(expirydate);
+var todaysDate = new Date();
+
+if((inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)))
+{
+
+
+}
+else
+{
+
+alert(" not equal")
+}
+}})};
